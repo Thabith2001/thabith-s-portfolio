db83905f1b79914695a5b664e60504c87a01fc7b
thabith <thabith20010909@gmail.com>
2025-06-09 19:52:23 +0530

    Documents.js

---
+++ src/Data/Documents.js
@@ -0,0 +1,60 @@
+
+export const documents = [
+    {
+        title: "Bachelor's Degree Certificate",
+        description: "Verified Certificate document of Bachelor's degree.",
+        type: "image",
+        src: "/images/Certificate.jpg",
+        cover: "/images/Certificate.jpg",
+    },
+    {
+        title: "Final Year Degree Transcript",
+        description: "Verified transcript document of Bachelor's degree.",
+        type: "image",
+        src: "/images/Transcript.jpg",
+        cover: "/images/Transcript.jpg",
+    },
+    {
+        title: "Award Verification Letter",
+        description: "Important letter from the University.",
+        type: "image",
+        src: "/images/award.png",
+        cover: "/images/award.png",
+    },
+    {
+        title: "Letter of Recommendation",
+        description: "Important letter from a previous professor.",
+        type: "image",
+        src: "/images/img1.jpg",
+        cover: "/images/img1.jpg",
+    },
+    {
+        title: "Confirmation of Module Completion",
+        description: "Important letter from the Institute.",
+        type: "image",
+        src: "/images/img2.jpg",
+        cover: "/images/img2.jpg",
+    },
+    {
+        title: "Confirmation of Academic Details",
+        description: "Important letter from the Institute.",
+        type: "image",
+        src: "/images/img3.jpg",
+        cover: "/images/img3.jpg",
+    },
+    {
+        title: "Confirmation of Academic Achievements",
+        description: "Important letter from the Institute.",
+        type: "image",
+        src: "/images/img4.jpg",
+        cover: "/images/img4.jpg",
+    },
+    {
+        title: "Recommendation Letter for Masters",
+        description: "Important letter from the Institute.",
+        type: "image",
+        src: "/images/img5.jpg",
+        cover: "/images/img5.jpg",
+    },
+
+];
